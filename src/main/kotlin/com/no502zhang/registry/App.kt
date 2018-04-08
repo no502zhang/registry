8e8a780e89295feda8ab670d3d9a72f29bbbaaae
no502zhang <no502zhang@163.com>
2018-04-08 23:46:32 +0800

    registry service

---
+++ src/main/kotlin/com/no502zhang/registry/App.kt
@@ -0,0 +1,14 @@
+package com.no502zhang.registry
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
+
+@SpringBootApplication
+@EnableEurekaServer
+open class App
+
+fun main(args: Array<String>) {
+    SpringApplication.run(App::class.java, *args)
+}
+
