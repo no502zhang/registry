09a05b450d8f9b2b82f7f92d68f72bdc74251394
no502zhang <no502zhang@163.com>
2018-08-10 11:31:07 +0800

    升级springcloud2并增加kotlin allopen插件

--- src/main/kotlin/com/no502zhang/registry/App.kt
+++ src/main/kotlin/com/no502zhang/registry/App.kt
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
 @EnableEurekaServer
-open class App
+class App
 
 fun main(args: Array<String>) {
     SpringApplication.run(App::class.java, *args)
